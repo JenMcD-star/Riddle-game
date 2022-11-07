eba8df1c78527c8a7f7a7e9d1be4083fac1585e4
JenMcD-star <jenniferannvancura@gmail.com>
2022-11-06 18:01:51 -0600

    array version of riddle game

--- script.js
+++ script.js
@@ -6,19 +6,6 @@ through objects/array to display a question and the answer for the questions.
 Some practice with forms/event listeners if you want the user to put in an answer 
 and/or click a button to display the answers*/
 
-/* 
-create display
-make riddles and answers 
-display a riddle
-enter a guess
-save the guess in a variable
-check the guess against the answer
-if wrong display keep guessing
-if right add 1 to score (make a scoreboard)
-create a countdown
-when countdown is over display correct answer and disable more guesses
-create a next button for new riddle
-*/
 const riddleDisplay = document.getElementById("riddle");
 const checkAnswerBtn = document.getElementById("checkAnswer");
 const userGuess = document.getElementById("textBox");
@@ -26,8 +13,32 @@ const startBtn = document.getElementById("start");
 const wrongInput = document.getElementById("wrongInput");
 const wrongGuess = document.getElementById("wrongGuess");
 const nextBtn = document.getElementById("next");
-let riddle;
-let answer;
+let position = 0;
+
+let riddle = [
+  "What has to be broken before you can use it?",
+  "I’m tall when I’m young, and I’m short when I’m old. What am I?",
+  "What month of the year has 28 days?",
+  "What is full of holes but still holds water?",
+  "What question can you never answer yes to?",
+  "What is always in front of you but can’t be seen?",
+  "There’s a one-story house in which everything is yellow. Yellow walls, yellow doors, yellow furniture. What color are the stairs?",
+  "What can you break, even if you never pick it up or touch it?",
+  "What goes up but never comes down?",
+  "A man who was outside in the rain without an umbrella or hat didn’t get a single hair on his head wet. Why?",
+];
+let answer = [
+  "an egg",
+  "A candle",
+  "All of them",
+  "a sponge",
+  "Are you asleep yet?",
+  "The future",
+  "There aren’t any—it’s a one-story house.",
+  "A promise",
+  "your age",
+  "he was bald",
+];
 let p = document.createElement("p");
 let myTimer;
 
@@ -36,22 +47,19 @@ checkAnswerBtn.addEventListener("click", checkAnswer);
 nextBtn.addEventListener("click", getRiddle);
 
 function getRiddle() {
-resetTimer();
-  fetch("https://riddles-api.vercel.app/random", { mode: "cors" })
-    .then((response) => {
-      return response.json();
-    })
-    .then(function (response) {
-      riddle = response.riddle;
-      answer = response.answer;
-      console.log(answer);
-
-      p.innerHTML = `${riddle}`;
-      riddleDisplay.appendChild(p);
-      wrongGuess.innerHTML = "";
-    });
-  //start timer
-  startTimer();
+  resetTimer();
+  console.log(position)
+  if (position <= 8) {
+    let currentRiddle = riddle[position];
+    p.innerHTML = `${currentRiddle}`;
+    riddleDisplay.appendChild(p);
+    wrongGuess.innerHTML = "";
+    //start timer
+    startTimer();
+    position++;
+  } else {
+    position = 0;
+  }
 }
 //add code to make user guess and answer case and punctuation insensitive
 function checkAnswer() {
@@ -59,8 +67,11 @@ function checkAnswer() {
   let userAnswerLower = userAnswer.toLowerCase();
   let userAnswerFinal = userAnswerLower;
   userAnswerFinal = userAnswerFinal.replace(/[^a-zA-Z\d]/g, "");
-  let answerLower = answer.toLowerCase();
+  let currentAnswer = answer[position-1];
+  let answerLower = currentAnswer.toLowerCase();
   let answerFinal = answerLower;
+ 
+
   answerFinal = answerFinal.replace(/[^a-zA-Z\d]/g, "");
   let wrongtext = "Wrong guess. Guess again!";
   if (userAnswerFinal === answerFinal) {
@@ -75,26 +86,24 @@ function checkAnswer() {
 }
 
 function startTimer() {
-  let seconds = 59;
+  let seconds = 30;
   tick();
   function tick() {
     let countdown = document.getElementById("countdown");
     seconds--;
     countdown.innerHTML = "0:" + (seconds < 10 ? "0" : "") + String(seconds);
     if (seconds > 0) {
-     myTimer = setTimeout(tick, 1000);
+      myTimer = setTimeout(tick, 1000);
     } else {
-      p.innerHTML = `The answer is: ${answer}`;
+      let currentAnswer = answer[position-1];
+      p.innerHTML = `The answer is: ${currentAnswer}`;
       riddleDisplay.appendChild(p);
       wrongGuess.innerHTML = "Time is up!";
-      riddle = null;
-      answer = null;
-      
     }
   }
 }
 function resetTimer() {
   let countdown = document.getElementById("countdown");
   countdown.innerHTML = "0:00";
-  clearTimeout(myTimer)
+  clearTimeout(myTimer);
 }
